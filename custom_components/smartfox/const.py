ff15f797e212040bbc5ca8fbfa01a16bd89ee2a0
Crafty-Codes <fabio.oberhofer@gmx.net>
2023-05-29 18:23:38 +0200

    src: fix missing list

--- custom_components/smartfox/const.py
+++ custom_components/smartfox/const.py
@@ -9,15 +9,15 @@ CONF_HOST = "host"
 
 DEFAULT_HOST = "http://smartfox/values.xml"
 
-SENSORE = list()
+SENSORS = list()
 
-SENSORE.append(SmartfoxData(
+SENSORS.append(SmartfoxData(
         name="To Grid",
         key="toGridValue",
         native_unit_of_measurement=UnitOfPower.KILO_WATT,
         change=lambda value : float(value[:-3])
     ))
-SENSORE.append(SmartfoxData(
+SENSORS.append(SmartfoxData(
         name="Production",
         key="hidProduction",
         native_unit_of_measurement=UnitOfPower.KILO_WATT,
